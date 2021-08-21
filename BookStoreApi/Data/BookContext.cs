ba45a03a7d4e30df5970246763c0642a6daccd95
KhaledYussef <70583586+KhaledYussef@users.noreply.github.com>
2021-08-21 19:06:43 +0300

    Add project files.

---
+++ BookStoreApi/Data/BookContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApi.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+        }
+    }
+}
